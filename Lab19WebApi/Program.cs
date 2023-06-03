6bd6e39ce817ae40c91a636f5f28f48ac9e08019
alexandra.kerezsi.18@gmail.com <alexandra.kerezsi.18@gmail.com>
2023-06-03 17:04:47 +0300

    Reconfigurat documentatia Swagger

--- Lab19WebApi/Program.cs
+++ Lab19WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,13 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o => AddSwaggerDocumentation(o));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
+{ 
     app.UseSwagger();
     app.UseSwaggerUI();
 }
@@ -23,3 +26,9 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void AddSwaggerDocumentation(SwaggerGenOptions o)
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    o.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+}
